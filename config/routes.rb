300b6ca8a268303438082000275d61e75f929214
Ehsan <valizade@mail.gvsu.edu>
2017-05-07 18:25:43 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  root 'application#hello'
+  root :to => redirect('users')
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 
 end

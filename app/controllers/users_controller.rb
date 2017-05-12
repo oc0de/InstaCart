2653700b834bccf04c3bdf13fde56db428c5c6b9
Ehsan <valizade@mail.gvsu.edu>
2017-05-12 02:12:36 -0500

    add update profile / add instacart header logo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,12 +25,27 @@ class UsersController < ApplicationController
   def welcome
     @user = User.find(params[:id])
     if params[:status]
+      flash[:success] = "Welcome to the instacart!"
       log_in @user
       @user.permission = true
       @user.save
     else
       @user.delete
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 end
